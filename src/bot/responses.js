51d87905408fe829f71bb1587bc94524da8448cc
Georgy Marrero <georgymarrero@gmail.com>
2017-02-19 00:03:39 -0800

    clear everything

--- src/bot/responses.js
+++
@@ -1,5 +0,0 @@
-export default {
-  'Hello': 'Hey!',
-  "What's your name?": 'Mr. Orange',
-  "Are you a robot?": "no."
-}
--- src/config.js
+++
@@ -1,6 +0,0 @@
-var messengerPageToken = process.env.MESSENGER_TOKEN || 'EAAUaNnBkuR4BAN12Ke8qTOuj6Hubq8qKSOZAEpr191xLuFb3NwWGZAh0ZB4aRsBZAhnzAoqXDSbJpPsKHZAmqvLFbILVs62l7sF4jrIOupC3qabWsrZBb7xZB0iO5Fc71mFA2UUMCXaO3GUPWusR0e5ZBa9hBkA52IvDAewuTUyDRQZDZD';
-
-module.exports = {
-  messengerPageToken: messengerPageToken,
-  messengerVerifyToken: 'thisisatest'
-};
